0bbb29e1874a47b83246ab8117d1c1b1979dfa6e
linash98 <103258558+linash98@users.noreply.github.com>
2024-02-26 14:16:53 -0500

    Add files via upload

---
+++ js/translations.js
@@ -0,0 +1,30 @@
+const translations = {
+  en: {
+    //index page
+    home: "Home",
+    About: "About",
+    Contact: "Contact us",
+    Work: "  Work",
+    english: "English",
+    arabic: "Arabic",
+    header_title:"DATA-DRIVEN CREATIVES",
+    header_title2:"CRAFTING HUMAN CONNECTION THROUGH DIGITAL EXPERIENCE",
+    header_paragraph:"Step Forward is a strategic digital agency powered by data analytics, market research, and digital technology"
+
+  },
+  ar: {
+    //index page
+    home: "الصفحة الرئيسية",
+    About: "من نحن",
+    Contact: "تواصل معنا",
+    Work: " أعمالنا",
+    english: "الانجليزية",
+    arabic: "العربية",
+    header_title:"إبداعات قائمة على البيانات",
+    header_title2:"صُناعة التواصل البشري عبر التجربة الرقمية",
+    header_paragraph:"سْتيب فوروارد هي وكالة رقمية استراتيجية مدعومة بتحليلات البيانات، وأبحاث السوق، والتكنولوجيا الرقمية",
+
+  },
+};
+
+export default translations;
